88d75c272d8d2fc4929f29e6dd84c8d0d5d3e057
Erick Barros <erickbarros311@gmail.com>
2025-05-19 23:47:37 -0300

    Application properties update, and Emtity game created

--- src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
+++ src/test/java/com/devsuperior/dslist/DslistApplicationTests.java
@@ -1,4 +1,4 @@
-package com.devesuperior.dslist;
+package com.devsuperior.dslist;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DslistApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
